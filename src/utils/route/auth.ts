a770cd9a9c39801e2a760f9576999bf9b50025cb
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2021-06-23 03:18:02 +0800

    chore: Minimal code cleanup

--- src/utils/route/auth.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This script includes authentication related utility functions
- */
-export type validationErrorType = { validation: { dataPath: string, keyword: string }[] };
-
-export const passwordValidationError = (error: Error & validationErrorType) => (error.validation
-  .find(({ dataPath, keyword }) => dataPath === '.password' && keyword === 'pattern')
-  ? new Error('Password pattern error!') : error);
